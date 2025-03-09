004088491a038306f289bccda153352dbbec0c84
nathan-nigussie <nathantolosa11@gmail.com>
2025-03-09 20:24:19 +0200

    file update

--- routes/routeTasks.js
+++ routes/routeTasks.js
@@ -8,7 +8,7 @@ router.route('/').get(getAllTasks).post(createTask)
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 //reply single task with its id
 router.route('/:id').get(getTask)
-router.route('/v2/abbe').get(getAllTasks)
+
 
 
 module.exports=router
