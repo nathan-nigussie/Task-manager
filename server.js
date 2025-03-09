2be5b7f019016e484ec4b1c922411ff876bb4c31
nathan-nigussie <nathantolosa11@gmail.com>
2025-03-09 14:45:46 +0200

    server file updated

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const connectDB=require('./db/connect');
+const connectDB=require('./server/db/connect');
 const express=require('express');
 const app=express();
-const tasks=require('./routes/routeTasks');
-const notFound=require('./middleware/not-found');
+const tasks=require('./server/routes/routeTasks');
+const notFound=require('./server/middleware/not-found');
 require('dotenv').config();
 
 // Middleware 
 app.use(express.json());
-app.use(express.static('../public'));
+app.use(express.static('./public'));
 
 // Root routes
 app.use('/api/v1/tasks',tasks);
 app.use(notFound);
 
-const port=process.env.PORT||3000; 
+const port=process.env.PORT||3000;
 
 const start=async () => {
     try {
