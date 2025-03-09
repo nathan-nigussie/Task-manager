5447a5ab922adc0ead4960f60b925959131ddd28
nathan-nigussie <nathantolosa11@gmail.com>
2025-03-09 20:21:11 +0200

    file update

--- routes/routeTasks.js
+++ routes/routeTasks.js
@@ -6,7 +6,9 @@ const {getAllTasks,createTask,updateTask,deleteTask,getTask}=require('../control
 router.route('/').get(getAllTasks).post(createTask)
 
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
-//reply single task with its id 
+//reply single task with its id
 router.route('/:id').get(getTask)
+router.route('/v2/abbe').get(getAllTasks)
+
 
 module.exports=router
